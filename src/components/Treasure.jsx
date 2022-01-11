ed166b4615f5410a7530c5ac51db12a2aab5ce9d
robin00007 <76676218+robin00007@users.noreply.github.com>
2022-01-12 00:23:01 +0530

    leaderboard added

--- src/components/Treasure.jsx
+++ src/components/Treasure.jsx
@@ -1,14 +1,41 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
 import "../styles/css/treasure.css";
 import treasure from "../styles/images/Treasure.png";
+import poster from "../styles/images/winner.png";
 
 const Treasure = () => {
-	return (
-		<div className="treasure-section">
-			<img src={treasure} alt="" />
-			<p>Coming Soon...</p>
-		</div>
-	);
+  const [list, setList] = useState([
+    { No: 1, TeamName: "something", status: "submitted" },
+    { No: 2, TeamName: "something2", status: "submitted2" },
+    { No: 3, TeamName: "something3", status: "submitted3" },
+    { No: 4, TeamName: "something4", status: "submitted4" },
+  ]);
+
+  return (
+    <div className="treasure-section">
+      <div className="banner">
+        <div className="poster2">
+          <img src={poster} alt="" />
+          <h1>APP Dev Contest 2.0</h1>
+        </div>
+      </div>
+      <div className="leaderboard">
+        <h1>Leaderboard</h1>
+        <h1>APP Dev Contest 2.0</h1>
+
+        {list.map((e) => {
+          return (
+            <div className="listItem">
+              <p>{e.No}</p>
+              <p>{e.TeamName}</p>
+              <p>{e.status}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Treasure;
